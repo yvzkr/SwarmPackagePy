6505eb93364a88abb8a57fe6b2123663825c9c5d
yvzkr <yavuzkuru3@gmail.com>
2019-12-02 14:34:14 +0300

    change example algoritm type

--- example.py
+++ example.py
@@ -3,8 +3,6 @@ from SwarmPackagePy import testFunctions as tf
 from SwarmPackagePy import animation, animation3D
 
 
-alh = SwarmPackagePy.pso(50, tf.easom_function, -10, 10, 2, 20,
-                         w=0.5, c1=1, c2=1)
+alh = SwarmPackagePy.fwa(50, tf.easom_function, -10, 10, 2, 20)
 animation(alh.get_agents(), tf.easom_function, -10, 10)
 animation3D(alh.get_agents(), tf.easom_function, -10, 10)
-
